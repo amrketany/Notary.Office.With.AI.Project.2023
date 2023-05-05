7048a21c3061893a952761ffb11bd1c7cd247826
Eslam <email@email.com>
2023-05-06 00:27:44 +0200

    adding a page

--- src/Components/About/Icons.js
+++ src/Components/About/Icons.js
@@ -3,6 +3,27 @@ import './Icons.css';
 import './css/all.min.css';
 
 class Icon extends comp {
+
+    componentDidMount() {
+        document.getElementsByClassName('fa-facebook')[0].onclick = () => {
+            window.location.href = "https://www.facebook.com";
+        }
+        document.getElementsByClassName('fa-instagram')[0].onclick = () => {
+            window.location.href = "https://www.instagram.com";
+        }
+        document.getElementsByClassName('fa-youtube')[0].onclick = () => {
+            window.location.href = "https://www.youtube.com";
+        }
+        document.getElementsByClassName('fa-twitter')[0].onclick = () => {
+            window.location.href = "https://www.twitter.com";
+        }
+        document.getElementsByClassName('fa-pinterest')[0].onclick = () => {
+            window.location.href = "https://www.pinterest.com";
+        }
+        document.getElementsByClassName('fa-linkedin')[0].onclick = () => {
+            window.location.href = "https://www.linkedin.com";
+        }
+    }
     render() {
         return (
             <div className='cont2'>
