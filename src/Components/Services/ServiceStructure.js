176f3e2d5d4d6cfa62247b21b1a9cbcc581ffefa
Amr <A@gmail.com>
2023-05-06 20:38:35 +0300

    Edit title and logo icon

--- src/Components/Services/ServiceStructure.js
+++ src/Components/Services/ServiceStructure.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./ServiceStyle.module.css";
 
 
@@ -9,11 +9,47 @@ import { faCircleArrowDown } from '@fortawesome/free-solid-svg-icons';
 
 
 <link rel="stylesheet" href="../CSS/all.min.css"></link>
-
 //type...of contract
 //contractlink
 
+
 const ServiceStrucure = () => {
+    const formData = new FormData();
+    //handel file change function
+    function handelFileChange(e) {
+        console.log(e.target.files[0])
+        if (e.target && e.target.files[0]){
+            formData.append('file', e.target.files[0]);
+        }
+    }
+    //handel button onclick api submit
+    function handelApiOnSubmit() {
+        fetch("http://localhost:8000/customerSignUp", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                formData
+            })
+
+            // it is another way may be true 
+            // body:  formData
+        }).then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+            })
+    }
+
+//  //Validate-Function
+//     const validate = (value) => {
+//         const errors = {};
+//         if (!value.file) {
+//             errors.file = "File  Is Required";
+//         }
+//     }
+//   //End-of-validation
+
     //Links
 const link ="https://rern.gov.eg/pages/requests/2";
     return (
@@ -25,17 +61,17 @@ const link ="https://rern.gov.eg/pages/requests/2";
                     </p>
                 </div>
                 <div className={styles.get_file}>
-                    <h4> To Get Your File Follow This Link <a href={link}  target="_blank" rel="noreferrer">Go There</a></h4>
+                    <h4> To Get Your File Follow This Link <a href={link} target="_blank" rel="noreferrer">Go There</a></h4>
                 </div>
                 <div className={styles.upload}>
                     <h4>After Filling The File Upload It from Here</h4>
                     <FontAwesomeIcon icon={faCircleArrowDown} className={styles.icon} />
-                    
+                    {/* Start upload your file */}
                     <div className={styles.input_group}>
-                        <input type="file" className={styles.form_control} placeholder="Choose File" />
-                        <input className={styles.uploadbtn} type="submit" value="Submit" />
+                        <input type="file" className={styles.form_control} placeholder="Choose File" name="file" onChange={handelFileChange}/>
+                        <button className={styles.uploadbtn} onClick={handelApiOnSubmit}>Submit</button>
                     </div>
-
+                    
                 </div>
             </div>
         </div>
--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -14,63 +14,64 @@ const Services = () => {
                over tells the browser to make sure the image always covers the entire container, even if it has to stretch the image or cut a little bit off one of the edges. contain , on the other hand, says to always show the whole image, even if that leaves a little space to the sides or bottom.over tells the browser to make sure the image always covers the entire container, even if it has to stretch the image or cut a little bit off one of the edges. contain , on the other hand, says to always show the whole image, even if that leaves a little space to the sides or bottom.
             </p>
          </div>
+         {/* // start Slider */}
          <div className={styles.sliderCountainer}>
-         <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-inner">
-               <div class="carousel-item active" >
-                  <div className={styles.forBackgroundImage0}>
-                     <div className={styles.forBackgroundColor}>
-                        <div className={styles.sliderContent}>
-            <h2>All Machines and Buildings Services</h2>
-                        <p>over tells the browser to make sure the image always covers the entire container, even if it has to stretch the image or cut a little bit off one of the edges. contain , on the other hand, says to always show the whole image, even if that leaves a little space to the sides or bottom.</p>
+            <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+               <div class="carousel-inner">
+                  <div class="carousel-item active" >
+                     <div className={styles.forBackgroundImage0}>
+                        <div className={styles.forBackgroundColor}>
+                           <div className={styles.sliderContent}>
+                              <h2>All Machines and Buildings Services</h2>
+                              <p>over tells the browser to make sure the image always covers the entire container, even if it has to stretch the image or cut a little bit off one of the edges. contain , on the other hand, says to always show the whole image, even if that leaves a little space to the sides or bottom.</p>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="carousel-item " >
+                     <div className={styles.forBackgroundImage1}>
+                        <div className={styles.forBackgroundColor}>
+                           <div className={styles.sliderContent}>
+                              <h2>Rental any Building</h2>
+                              <p>over tells the browser to make sure the image always covers the entire container, even if it has to stretch the image or cut a little bit off one of the edges. contain , on the other hand, says to always show the whole image, even if that leaves a little space to the sides or bottom.</p>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="carousel-item " >
+                     <div className={styles.forBackgroundImage2}>
+                        <div className={styles.forBackgroundColor}>
+                           <div className={styles.sliderContent}>
+                              <h2>Buy and Sell any Building</h2>
+                              <p>over tells the browser to make sure the image always covers the entire container, even if it has to stretch the image or cut a little bit off one of the edges. contain , on the other hand, says to always show the whole image, even if that leaves a little space to the sides or bottom.</p>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="carousel-item " >
+                     <div className={styles.forBackgroundImage3}>
+                        <div className={styles.forBackgroundColor}>
+                           <div className={styles.sliderContent}>
+                              <h2>Buy and Sell any Veichle</h2>
+                              <p>over tells the browser to make sure the image always covers the entire container, even if it has to stretch the image or cut a little bit off one of the edges. contain , on the other hand, says to always show the whole image, even if that leaves a little space to the sides or bottom.</p>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="carousel-item " >
+                     <div className={styles.forBackgroundImage4}>
+                        <div className={styles.forBackgroundColor}>
+                           <div className={styles.sliderContent}>
+                              <h2>Rental any Veichle</h2>
+                              <p>over tells the browser to make sure the image always covers the entire container, even if it has to stretch the image or cut a little bit off one of the edges. contain , on the other hand, says to always show the whole image, even if that leaves a little space to the sides or bottom.</p>
+                           </div>
                         </div>
                      </div>
                   </div>
                </div>
-               <div class="carousel-item active" >
-                  <div className={styles.forBackgroundImage1}>
-                     <div className={styles.forBackgroundColor}>
-                        <div className={styles.sliderContent}>
-            <h2>Rental any Building</h2>
-                        <p>over tells the browser to make sure the image always covers the entire container, even if it has to stretch the image or cut a little bit off one of the edges. contain , on the other hand, says to always show the whole image, even if that leaves a little space to the sides or bottom.</p>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-               <div class="carousel-item active" >
-                  <div className={styles.forBackgroundImage2}>
-                     <div className={styles.forBackgroundColor}>
-                        <div className={styles.sliderContent}>
-            <h2>Buy and Sell any Building</h2>
-                        <p>over tells the browser to make sure the image always covers the entire container, even if it has to stretch the image or cut a little bit off one of the edges. contain , on the other hand, says to always show the whole image, even if that leaves a little space to the sides or bottom.</p>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-               <div class="carousel-item active" >
-                  <div className={styles.forBackgroundImage3}>
-                     <div className={styles.forBackgroundColor}>
-                        <div className={styles.sliderContent}>
-            <h2>Buy and Sell any Veichle</h2>
-                        <p>over tells the browser to make sure the image always covers the entire container, even if it has to stretch the image or cut a little bit off one of the edges. contain , on the other hand, says to always show the whole image, even if that leaves a little space to the sides or bottom.</p>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-               <div class="carousel-item active" >
-                  <div className={styles.forBackgroundImage4}>
-                     <div className={styles.forBackgroundColor}>
-                        <div className={styles.sliderContent}>
-            <h2>Rental any Veichle</h2>
-                        <p>over tells the browser to make sure the image always covers the entire container, even if it has to stretch the image or cut a little bit off one of the edges. contain , on the other hand, says to always show the whole image, even if that leaves a little space to the sides or bottom.</p>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-             </div>
             </div>
          </div>
-
+{/* //End Slider */}
 
 
          <div className={styles.Service}>
@@ -91,7 +92,7 @@ const Services = () => {
          </div>
       </div>
             
-   );
+   )
 }
 export default Services;
 
