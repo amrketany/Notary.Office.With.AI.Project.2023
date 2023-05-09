f91ef9edca413f31ad561a38781bedb3a8606828
Eslam <email@email.com>
2023-05-09 23:13:26 +0200

    abc

---
+++ src/Components/Services/Building/RentalBuilding.js
@@ -0,0 +1,13 @@
+import styles from "../ServiceStyle.module.css";
+import { Fragment } from "react";
+import ServiceStrucure from "../ServiceStructure";
+
+const RentalBuilding = () => {
+ return (
+  <div className={styles.main}>
+   <ServiceStrucure/>
+  </div>
+ )
+}
+
+export default RentalBuilding;
